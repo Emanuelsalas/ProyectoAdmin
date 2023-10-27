f1a7e71992ea51969cc035d25a8083d1415ab1e8
Rocamr <100881982+Rocamr@users.noreply.github.com>
2023-10-27 08:13:55 -0600

    firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,23 +1,31 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getStorage} from"firebase/storage";
+import { v4 } from "uuid";
+import { upload } from "@testing-library/user-event/dist/upload";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAfMRrz0ykMEtM7JEr8MFQ-DlNPIIYspUY",
-  authDomain: "homaspuntoventa.firebaseapp.com",
-  projectId: "homaspuntoventa",
-  storageBucket: "homaspuntoventa.appspot.com",
-  messagingSenderId: "90169124139",
-  appId: "1:90169124139:web:6c22f7082679da084cd16e",
-  measurementId: "G-KBB687T0E8"
+const configPVH = {
+  apiKey : process.env.REACT_APP_APIKEYPVH,
+  authDomain: process.env.REACT_APP_ADOMAINPVH,
+  projectId: process.env.REACT_APP_PROYECIDPVH,
+  storageBucket: process.env.REACT_APP_STORAGEPVH,
+  messagingSenderId: process.env.REACT_APP_SENDERIDPVH,
+  appId: process.env.REACT_APP_APPIDPVH,
+  measurementId: process.env.REACT_APP_MESUREIDPVH
 };
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export default app;
+const appPVH = initializeApp(configPVH);
+const analyticsPVH = getAnalytics(appPVH);
+export async function guardarFoto(archivo){
+  const dato = Storage(`/ImagenesSede/${v4()}`);
+  const snapshot = await dato.put(archivo);
+  // Obtener la URL de descarga de la imagen
+  const imagenURL = await snapshot.ref.getDownloadURL();
+  return imagenURL;
+}
+export default appPVH;
